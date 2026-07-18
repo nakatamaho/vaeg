dee4ce6047140336e12974968c7108627b365c6f
Nakata Maho <maho.nakata@gmail.com>
2026-07-18 14:58:30 +0900

    M46: enforce post-init dispatch immutability

--- i286c/v30patch.h
+++ i286c/v30patch.h
@@ -1,3 +1,9 @@
 
 void v30cinit(void);
 
+#if defined(VAEG_UPD9002_M46_TESTING)
+int upd9002_dispatch_test_verify(void);
+void upd9002_dispatch_test_require_immutable(void);
+UINT upd9002_dispatch_test_construction_count(void);
+UINT upd9002_dispatch_test_rejected_count(void);
+#endif
