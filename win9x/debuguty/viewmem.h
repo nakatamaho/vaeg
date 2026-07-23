31f5fc59ac46c7b7c9ca6495c875e923a9e8785b
Nakata Maho <maho.nakata@gmail.com>
2026-07-23 18:42:25 +0900

    M57: remove frozen reference tier

--- win9x/debuguty/viewmem.h
+++
@@ -1,2 +0,0 @@
-void viewmem_read(VIEWMEM_T *cfg, DWORD adrs, BYTE *buf, DWORD size);
-void viewmem_write(VIEWMEM_T *cfg, DWORD adrs, BYTE *buf, DWORD size);
--- win9x/debuguty/viewvabank.h
+++
@@ -1,4 +0,0 @@
-LRESULT CALLBACK viewvabank_proc(NP2VIEW_T *view,
-								HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
-
-void viewvabank_init(NP2VIEW_T *dst, NP2VIEW_T *src);
