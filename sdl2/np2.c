779a14dbc765e59ea6acc977008133331d64de1e
Nakata Maho <maho.nakata@gmail.com>
2026-07-28 09:33:51 +0900

    M65b: correct BOUND signed range decision

--- sdl2/np2.c
+++ sdl2/np2.c
@@ -84,6 +84,9 @@
 #if defined(VAEG_UPD9002_M65A_TESTING)
 #include	"tests/upd9002/m65a_ff7.h"
 #endif
+#if defined(VAEG_UPD9002_M65B_TESTING)
+#include	"tests/upd9002/m65b_bound.h"
+#endif
 #if defined(VAEG_UPD9002_SSTS_TESTING)
 #include	"tests/upd9002/ssts_worker.h"
 #endif
@@ -1475,6 +1478,11 @@ int main(int argc, char **argv) {
 #if defined(VAEG_UPD9002_M65A_TESTING)
 	if ((argc == 2) && !strcmp(argv[1], "--upd9002-m65a-ff7")) {
 		return upd9002_m65a_ff7_main();
+	}
+#endif
+#if defined(VAEG_UPD9002_M65B_TESTING)
+	if ((argc == 2) && !strcmp(argv[1], "--upd9002-m65b-bound")) {
+		return upd9002_m65b_bound_main();
 	}
 #endif
 #if defined(VAEG_UPD9002_SSTS_TESTING)
