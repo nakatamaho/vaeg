93f7e7994dbec08340773e12a2a7c1f9ec2f5d8a
Nakata Maho <maho.nakata@gmail.com>
2026-08-01 22:37:40 +0900

    M75: implement SCSI command phases and media geometry

--- cbus/scsicmd.c
+++ cbus/scsicmd.c
@@ -26,27 +26,73 @@ static const BYTE hdd_inquiry[0x20] = {
 			0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
 
 
+static void scsicmd_putbe32(BYTE *dst, UINT32 value) {
+
+	dst[0] = (BYTE)(value >> 24);
+	dst[1] = (BYTE)(value >> 16);
+	dst[2] = (BYTE)(value >> 8);
+	dst[3] = (BYTE)value;
+}
+
 static UINT scsicmd_datain(SXSIDEV sxsi, BYTE *cdb) {
 
 	UINT	length;
+	UINT	copylen;
+	UINT32	last_lba;
 
+	ZeroMemory(scsiio.data, sizeof(scsiio.data));
+	scsiio.data_len = 0;
 	switch(cdb[0]) {
 		case 0x12:				// Inquiry
 			TRACEOUT(("Inquiry"));
-			// Logical unit number = cdb[1] >> 5;
-			// EVPD = cdb[1] & 1;
-			// Page code = cdb[2];
+			// Logical unit number = cdb[1] >> 5.
+			// EVPD = cdb[1] & 1.
+			// Page code = cdb[2].
 			length = cdb[4];
-			if (length) {
-				CopyMemory(scsiio.data, hdd_inquiry, min(length, 0x20));
+			copylen = min(length, (UINT)sizeof(hdd_inquiry));
+			if (copylen) {
+				CopyMemory(scsiio.data, hdd_inquiry, copylen);
 			}
+			scsiio.data_len = copylen;
+			break;
+
+		case 0x25:				// Read Capacity (10)
+			TRACEOUT(("Read Capacity"));
+			last_lba = (sxsi->totals > 0) ? (UINT32)(sxsi->totals - 1) : 0;
+			scsicmd_putbe32(scsiio.data + 0, last_lba);
+			scsicmd_putbe32(scsiio.data + 4, sxsi->size);
+			scsiio.data_len = 8;
+			break;
+
+		case 0x1a:				// Mode Sense (6)
+			TRACEOUT(("Mode Sense (6)"));
+			/*
+			 * Return one direct-access block descriptor.  The device
+			 * geometry comes from the mounted SCSI image, while the
+			 * caller's allocation length controls the visible prefix.
+			 */
+			length = cdb[4];
+			scsiio.data[1] = 0x00;
+			scsiio.data[2] = 0x00;
+			scsiio.data[3] = 0x08;
+			scsicmd_putbe32(scsiio.data + 4,
+				(sxsi->totals > 0) ? (UINT32)sxsi->totals : 0);
+			scsiio.data[4] = 0;
+			scsiio.data[5] = (BYTE)(sxsi->totals >> 16);
+			scsiio.data[6] = (BYTE)(sxsi->totals >> 8);
+			scsiio.data[7] = (BYTE)sxsi->totals;
+			scsiio.data[8] = (BYTE)(sxsi->size >> 16);
+			scsiio.data[9] = (BYTE)(sxsi->size >> 8);
+			scsiio.data[10] = (BYTE)sxsi->size;
+			copylen = min(length, (UINT)12);
+			scsiio.data[0] = (copylen > 0) ? (BYTE)(copylen - 1) : 0;
+			scsiio.data_len = copylen;
 			break;
 
 		default:
-			length = 0;
+			break;
 	}
-	(void)sxsi;
-	return(length);
+	return(scsiio.data_len);
 }
 
 
@@ -83,7 +129,6 @@ REG8 scsicmd_select(REG8 id) {
 REG8 scsicmd_transfer(REG8 id, BYTE *cdb) {
 
 	SXSIDEV	sxsi;
-	UINT	leng;
 
 	if (scsiio.reg[SCSICTR_TARGETLUN] & 7) {
 		return(0x42);
@@ -100,15 +145,9 @@ REG8 scsicmd_transfer(REG8 id, BYTE *cdb) {
 			return(0x16);		// Succeed
 
 		case 0x12:				// Inquiry
-			leng = scsicmd_datain(sxsi, cdb);
-#if 0
-			if (leng > scsiio.transfer) {
-				return(0x2b);		// Abort
-			}
-			else if (leng < scsiio.transfer) {
-				return(0x20);		// Pause
-			}
-#endif
+		case 0x1a:				// Mode Sense (6)
+		case 0x25:				// Read Capacity (10)
+			scsicmd_datain(sxsi, cdb);
 			return(0x16);			// Succeed
 	}
 
@@ -131,11 +170,15 @@ static REG8 scsicmd_cmd(REG8 id) {
 	}
 	switch(scsiio.cmd[0]) {
 		case 0x00:
+			scsiio.phase = SCSIPH_STATUS;
 			return(0x8b);		// Transfer Status要求
 
 		case 0x12:				// inquiry
+		case 0x1a:				// Mode Sense (6)
+		case 0x25:				// Read Capacity (10)
 			scsicmd_datain(sxsi, scsiio.cmd);
 			scsiio.phase = SCSIPH_DATAIN;
+			scsiio.rddatpos = 0;
 			return(0x89);		// Transfer Data要求
 	}
 
@@ -151,6 +194,42 @@ BOOL scsicmd_send(void) {
 			return(SUCCESS);
 	}
 	return(FAILURE);
+}
+
+REG8 scsicmd_transinfo(REG8 id) {
+
+	REG8	ret;
+
+	switch(scsiio.phase) {
+		case SCSIPH_COMMAND:
+			CopyMemory(scsiio.cmd, scsiio.reg + SCSICTR_CDB,
+																	 sizeof(scsiio.cmd));
+			ret = scsicmd_cmd(id);
+			return(ret);
+
+		case SCSIPH_DATAIN:
+			if (scsiio.rddatpos >= scsiio.data_len) {
+				scsiio.phase = SCSIPH_STATUS;
+				return(0x8b);			}
+			return(0x89);			// Transfer Data request remains active.
+
+		case SCSIPH_DATAOUT:
+			if (scsiio.wrdatpos >= scsiio.data_len) {
+				scsiio.phase = SCSIPH_STATUS;
+				return(0x8b);
+			}
+			return(0x88);			// Transfer Data Out request.
+
+		case SCSIPH_STATUS:
+			scsiio.reg[SCSICTR_STATUS] = 0x00;
+			scsiio.phase = SCSIPH_MSGIN;
+			return(0x8f);			// Transfer Message request.
+
+		case SCSIPH_MSGIN:
+			scsiio.phase = 0;
+			return(0x80);			// Disconnect.
+	}
+	return(0x42);
 }
 
 
--- cbus/scsicmd.h
+++ cbus/scsicmd.h
@@ -6,6 +6,7 @@ extern "C" {
 REG8 scsicmd_negate(REG8 id);
 REG8 scsicmd_select(REG8 id);
 REG8 scsicmd_transfer(REG8 id, BYTE *cdb);
+REG8 scsicmd_transinfo(REG8 id);
 BOOL scsicmd_send(void);
 
 
