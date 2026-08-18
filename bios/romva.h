e311ea583397f04d9489e82897405fe139e2c3aa
Nakata Maho <maho.nakata@gmail.com>
2026-08-18 22:45:09 +0900

    M96i1: select VA default font

--- bios/romva.h
+++ bios/romva.h
@@ -4,6 +4,8 @@ extern "C" {
 #endif
 
 void romva_initialize(void);
+const char *romva_default_font_filename(void);
+BOOL romva_load_default_font(void);
 BOOL romva_load_pc98_font(const char *filename);
 
 #ifdef __cplusplus
